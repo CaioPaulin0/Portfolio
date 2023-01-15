1f2b9abb261569a342c76203e04ee03f825b04fc
thegamedenerd <gamedenerd@hotmail.com>
2023-01-15 20:45:59 -0300

    js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 
+import data from "../projeto.json" assert {type: "json"}
+
+
+
 const toggle = document.getElementById('toggle')
 const div = document.querySelector('.headerCont')
 
@@ -7,7 +11,128 @@ toggle.addEventListener('click', () => {
     div.classList.toggle('active')
 })
 
-backAmenu = () => {
+function projeto(data){
+        const div = document.createElement('div')   
+        const img = document.createElement('img')
+        const fechar = document.createElement('p')
+        fechar.innerHTML = "X"
+        fechar.classList.add("fechar")
+
+        fechar.addEventListener('click', () => {
+            document.querySelector('main').style.display = "block"
+            document.querySelector('header').style.display = "flex"
+            document.querySelector('footer').style.display = "flex"
+
+            document.querySelector('body').removeChild(document.querySelector('.projeto'))
+        })
+        div.classList.add('projeto')
+        
+       img.setAttribute('src', data.gif)
+
+        const infoDiv = document.createElement('div')
+
+
+        
+        infoDiv.classList.add('projetoCont')
+        infoDiv.innerHTML = 
+        `<div class="infProjeto">
+
+
+        <img src= ${data.foto} >
+        <p> ${data.nome} </p>
+
+        <div class="tecProjeto">
+        <ul>
+            ${data.midia.map((data) => {
+                return `<li>${data.icon}</li>`
+            })}
+            </ul>
+        </div>
+
+        </div>
+        
+        <div class="descProjeto">
+          <span> Descrição </span>
+
+          <p>
+                ${data.descricao}
+          </p>
+        </div>
+
+        <div class="tecProjeto">
+            <span> Tecnologias </span>
+            <div>
+            <ul id="tec">
+
+            ${data.tecnologias.map((data) => {
+                return  data.logo
+            })}
+            
+            </ul>
+            </div>
+        </div>
+        `
+        div.appendChild(fechar)
+        div.appendChild(img)
+        div.appendChild(infoDiv)
+
+        const tec = document.querySelector('#tec')
+
+        console.log(tec)
+        return div
+}
+
+function divProjeto(data,jobs){
+    const div = document.createElement('div')
+
+    div.classList.add('projetoSlide')
+    div.setAttribute('data-jobs', jobs)
+
+    const img = document.createElement('img')
+    img.setAttribute("src", data.foto)
+
+    const divNome = document.createElement('div')
+    divNome.classList.add('textoProjeto')
+    const p = document.createElement('p')
+    p.innerHTML = data.nome
+
+    divNome.appendChild(p)
+
+    div.appendChild(img)
+    div.appendChild(divNome)
+
+    div.addEventListener('click', () => {
+
+        const main = document.querySelector('main')
+        const header = document.querySelector('header')
+        const footer = document.querySelector('footer')
+        const body = document.querySelector('body')
+        
+        main.style.display = "none"
+        header.style.display = "none"
+        footer.style.display = "none"
+        body.appendChild(projeto(data))
+
+        return 
+    })
+    return div
+}
+
+function listaProjetos(){
+
+    const projetosDiv = document.querySelector('.projetosAll')
+
+     data.map((data) => {
+        if(data.tipo === "profissional"){
+            projetosDiv.appendChild(divProjeto(data,"2"))
+            
+        }
+    })
+}
+
+listaProjetos()
+
+function backAmenu(){
     const headerCont = document.querySelector('.headerCont')
     const a = headerCont.querySelectorAll('a')
 
@@ -20,7 +145,7 @@ backAmenu = () => {
 
 backAmenu()
 
-remove = () => {
+function remove(){
     const array = document.querySelectorAll('[data-jobs]')
 
     array.forEach(cont => {
@@ -31,7 +156,7 @@ remove = () => {
     )
 }
 
-show = (valor) => {
+function  show(valor){
     const array = document.querySelectorAll(`[data-jobs="${valor}"]`)
 
     array.forEach(cont => cont.classList.remove('hiden'))
@@ -53,20 +178,8 @@ for(let i = 0; i < btnButton.length; i++){
     })
 }
 
-aniHabilidade = () => {
-    const hTextos = document.querySelector('.habilidadesTextos')
 
-    const iconsHabilidades = hTextos.querySelectorAll('i')
-
-    for(let i = 0; i < iconsHabilidades.length; i++){
-        setTimeout(() => {
-            iconsHabilidades[i].classList.add("iconHabilidades")
-            
-        }, [i * 100] )
-    }
-}
-
-aniWindow = () => {
+function  aniWindow(){
     const arrayW = document.querySelectorAll('[data-window]')
 
     const windowTop = window.pageYOffset + window.innerHeight * 0.8
@@ -74,9 +187,7 @@ aniWindow = () => {
     arrayW.forEach(element => {
         if(windowTop > element.offsetTop){
             element.classList.remove('hiden')
-            element.classList.add('animacao');
-            aniHabilidade()
-            
+            element.classList.add('animacao');          
         } 
     })
 }
