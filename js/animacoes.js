2c8a06cc79e5b9b377fa77c66318b6ce875e782f
Caio paulino <82124997+CaioPaulin0@users.noreply.github.com>
2025-05-06 20:48:53 -0300

    remover comentários

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,20 +1,17 @@
 const div = document.querySelector('.habCont');
 const proj = document.querySelector('.projetosCont');    
 const sites = document.querySelector('.sitesCont');    
-    // Função callback quando a div aparece na tela
     const callback = (entries, observer) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           entry.target.classList.add('add')
-          // Aqui você pode fazer algo com o valor da div
         }
       });
     };
 
-    // Configuração do IntersectionObserver
     const observer = new IntersectionObserver(callback, {
-      root: null, // Use o viewport como root
-      threshold: 0.2 // A div deve estar ao menos 10% visível para acionar o callback
+      root: null,
+      threshold: 0.2
     });
 
     // Observar a div
